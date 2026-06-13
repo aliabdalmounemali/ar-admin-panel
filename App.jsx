b04f8ab4721dc6de00842c4ffaef81c8e8b31745
aliabdalmounemali <aliabdalmnam290@gmail.com>
2026-06-13 14:39:10 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import './App.css';
 
 // 🟢 رابط Railway الدائم
-const API_URL = "https://ar-app-backend-production.up.railway.app";
+const API_URL = "https://ar-app-backend-production-1d91.up.railway.app";
 
 function App() {
   const [targets, setTargets] = useState([]);
