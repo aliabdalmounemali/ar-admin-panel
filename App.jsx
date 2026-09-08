dff047a67597376d8f444275f7307bed9a7039eb
aliabdalmounemali <aliabdalmnam290@gmail.com>
2026-09-09 00:16:32 +0300

    Fix API_URL to use HTTPS

--- App.jsx
+++ App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API_URL = "httpar-app-backend-production-de6e.up.railway.app";
+const API_URL = "https://ar-app-backend-production-de6e.up.railway.app";
 
 function App() {
   const [targets, setTargets] = useState([]);
